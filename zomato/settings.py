96a2a8cf914bfa31c5d6ad240330080cf3fcd3b9
Esai Keshav <magicesaik7@gmail.com>
2026-01-23 21:04:38 +0530

    Added pagination

--- zomato/settings.py
+++ zomato/settings.py
@@ -86,6 +86,8 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": [
         "rest_framework.filters.SearchFilter",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 16,
 }
 
 SPECTACULAR_SETTINGS = {
